19b63e83d19e6dad28347da4838673f9922fcc18
rashad ahmed <rashadahmed177@gmail.com>
2023-04-07 17:09:20 +0200

    initial commit and test

---
+++ text.py
@@ -0,0 +1,7 @@
+name=input("Enter Your Name , Please " ).title()
+age=input("What Is Your Age ?"
+if age>=18:
+    print(f"Hello {name} ")
+    print("Your Age Is compatable With Us ")
+else:
+    print("You Are Young")
